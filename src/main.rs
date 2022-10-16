21b3ac5d1177d0518e18a80cda9f0922d7da0d40
Gal Ofri <galmenashofri@gmail.com>
2022-10-16 11:42:56 +0300

    read forever

--- src/main.rs
+++ src/main.rs
@@ -24,9 +24,14 @@ fn main() {
     let config = get_config();
     let chunk_size = config.unwrap().get_int("chunk_size").unwrap() as u64;
     let mut cr = new_chunk_reader(args.path, chunk_size).unwrap();
-    for i in 1..10 {
+    let mut i = 0;
+    loop {
         let mut buf = vec![0u8; chunk_size as usize];
         let s = cr.read_chunk(i, &mut buf).unwrap();
+        if s == 0 {
+            break;
+        }
         println!("read {} bytes: {}", s, str::from_utf8(&buf).unwrap());
+        i += 1;
     }
 }
